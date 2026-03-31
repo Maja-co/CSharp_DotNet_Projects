6ddd29e9f85edad4679c3c5e3d8ea65c6bf15174
Maja <onlineshit61@gmail.com>
2026-03-31 11:41:13 +0200

    Maui opsætning

---
+++ MinForsteMauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MinForsteMauiApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MinForsteMauiApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MinForsteMauiApp;
+
+public partial class MainPage : ContentPage {
+    int count = 0;
+
+    public MainPage() {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e) {
+        
+    }
+}
