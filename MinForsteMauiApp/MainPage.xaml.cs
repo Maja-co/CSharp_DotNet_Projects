a0224def0562ac932eaf9a9fb603f5a3a47cf523
Maja <onlineshit61@gmail.com>
2026-04-02 10:09:27 +0200

    Afsluttet Lektion 12

--- MinForsteMauiApp/MainPage.xaml.cs
+++ MinForsteMauiApp/MainPage.xaml.cs
@@ -6,4 +6,26 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private void Radio_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value)
+        {
+            RadioButton radio = (RadioButton)sender;
+            RadioStatus.Text = "Vejr retning: " + radio.Content;
+        }
+    }
+
+    private void Check_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        string valgte = "";
+        if (LemonCheck.IsChecked) valgte += "Lemon, ";
+        if (OrangeCheck.IsChecked) valgte += "Orange, ";
+        if (BananaCheck.IsChecked) valgte += "Banana, ";
+
+        if (valgte == "")
+            CheckStatus.Text = "Frugt: ingen valgt";
+        else
+            CheckStatus.Text = "Frugt valgt: " + valgte.TrimEnd(',', ' ');
+    }
 }
